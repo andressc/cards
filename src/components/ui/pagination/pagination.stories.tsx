a5d6d7c9d4723620eae72e217708936e96648829
andrejrebrov <andressc@mail.ru>
2024-03-16 12:47:17 +0300

    refactoring

--- src/components/ui/pagination/pagination.stories.tsx
+++ src/components/ui/pagination/pagination.stories.tsx
@@ -14,6 +14,11 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
+  args: {
+    count: 10,
+    onChange: () => {},
+    page: 3,
+  },
   render: () => {
     const [page, setPage] = useState(1)
     const [perPage, setPerPage] = useState(10)
